bc290393171669e22b0fb672ee082ad9496aa356
Serga05 <88805832+Serga05@users.noreply.github.com>
2021-12-05 10:30:38 +0300

    Delete index.js

--- index.js
+++
@@ -1,4 +0,0 @@
-   function Kio(){
-   document.write('<a href="http://t.me/CryptoTab_Download_add"><title>Соц. сети</title><a href="https://serga05.github.io/LK/"><p><link rel="stylesheet" href="index.css" type="text/css"></link><button id="Button-Back"><-- Назад</button></p></a><div><button style="background-color: #7dd6ff; border-color: #7dd6ff">Телеграм - Канал</button></a>')
-   }
-
