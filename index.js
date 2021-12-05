97beec3673aae4c2dbb04e54ed1f4a30c095daac
Serga05 <88805832+Serga05@users.noreply.github.com>
2021-12-05 10:21:14 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+   function Kio(){
+   document.write('<a href="http://t.me/CryptoTab_Download_add"><a href="file:///C:/Users/Lenovo/Desktop/Сайты/index.html"><p><link rel="stylesheet" href="index.css" type="text/css"></link><button id="Button-Back"><-- Назад</button></p></a><div><button style="background-color: #7dd6ff; border-color: #7dd6ff">Телеграм - Канал</button></a>')
+   }
+   function LLL(){
+	   document.write('ХИХИ')
+   }
