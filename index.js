5ff20a2d7809e900f58ee053d8d33206cc7beb09
Serga05 <88805832+Serga05@users.noreply.github.com>
2021-12-05 10:22:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
    function Kio(){
-   document.write('<a href="http://t.me/CryptoTab_Download_add"><a href="file:///C:/Users/Lenovo/Desktop/Сайты/index.html"><p><link rel="stylesheet" href="index.css" type="text/css"></link><button id="Button-Back"><-- Назад</button></p></a><div><button style="background-color: #7dd6ff; border-color: #7dd6ff">Телеграм - Канал</button></a>')
+   document.write('<a href="http://t.me/CryptoTab_Download_add"><title>Соц. сети</title><a href="file:///C:/Users/Lenovo/Desktop/Сайты/index.html"><p><link rel="stylesheet" href="index.css" type="text/css"></link><button id="Button-Back"><-- Назад</button></p></a><div><button style="background-color: #7dd6ff; border-color: #7dd6ff">Телеграм - Канал</button></a>')
    }
-   function LLL(){
-	   document.write('ХИХИ')
-   }
+
